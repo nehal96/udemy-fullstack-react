732bfba662fe8e7df00941728911bd2339699ba7
Nehal Udyavar <nehaludyavar@gmail.com>
2018-11-23 19:17:40 -0800

    Add user to db once profile is received

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/User"); // Require models before passport
 require("./services/passport");
-require("./models/User");
 
 mongoose.connect(keys.mongoURI);
 
