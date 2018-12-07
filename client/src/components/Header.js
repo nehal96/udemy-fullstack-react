ea9715477f995abab20326b46c107f69eea8b83e
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-06 22:34:45 -0800

    Render header content based on auth state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        )
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        )
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,11 +29,7 @@ class Header extends Component {
             Emailify
           </a>
           <ul className="right">
-            <li>
-              <a>
-                Login with Google
-              </a>
-            </li>
+            { this.renderContent() }
           </ul>
         </div>
       </nav>
@@ -21,4 +37,10 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return {
+    auth
+  }
+}
+
+export default connect(mapStateToProps)(Header)
