98fdd88c694ee4cc42f377c50c53278fe957325b
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-17 21:57:49 -0800

    Ensuring Express serves up static React files in production

--- index.js
+++ index.js
@@ -24,5 +24,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets like main.js & main.css files
+  app.use(express.static('client/build'))
+
+  // Express will serve up index.html if it does not recognize route
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
