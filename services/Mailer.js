a2eff4a17d598ca09d24d9172a0fc0c3b7f11acd
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-19 01:49:17 -0800

    Finish up Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,10 +6,37 @@ class Mailer extends helper.Mail {
   constructor({ subject, recipients }, content) {
     super();
 
-    this.from_email = new helper.email("no-reply@emailify.com");
+    this.from_email = new helper.Email("no-reply@emailify.com");
     this.subject = subject;
     this.body = new helper.Content("text/html", content);
-    this.recipients = this.formatAddresses(recipients)
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body); // built-in function of Mail
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = helper.Personalization();
+
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
   }
 }
 
