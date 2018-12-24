e5e00ce075696e28faba265e1143f4f2507ce347
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-24 01:45:13 -0800

    Style changes to form and form buttons

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
 import { reduxForm, Field } from 'redux-form'
+import { Link } from 'react-router-dom'
 import SurveyField from './SurveyField'
 
 const FIELDS = [
@@ -31,7 +32,13 @@ class SurveyForm extends Component {
           onSubmit={ this.props.handleSubmit(values => console.log(values)) }
         >
           { this.renderFields() }
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat left white-text">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
