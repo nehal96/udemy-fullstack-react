a23c943d73aafd9996486aac3307a6db89bd6a28
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-23 22:20:01 -0800

    Create init. SurveyForm component + wire up to reduxForm

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,12 @@
+// SurveyNew shows SurveyNew and SurveyReview components
 import React, { Component } from 'react'
+import SurveyForm from './SurveyForm'
 
 class SurveyNew extends Component {
   render() {
     return (
       <div>
-        SurveyNew
+        <SurveyForm />
       </div>
     )
   }
