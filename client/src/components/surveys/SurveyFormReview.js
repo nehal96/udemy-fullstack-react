5b5685fc0b582262e59aca88c97aec59bd62e7ba
Nehal Udyavar <nehaludyavar@gmail.com>
2018-12-25 00:28:29 -0800

    Setting up send survey button and action creator

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
 import formFields from './formFields'
+import * as actions from '../../actions'
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
       <div key={ name }>
@@ -20,10 +21,17 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
       <h5>Please review your entries.</h5>
       { reviewFields }
       <button
-        className="yellow darken-3 btn-flat"
+        className="yellow darken-3 white-text btn-flat"
         onClick={ onCancel }
       >
         Back
+      </button>
+      <button
+        className="green white-text btn-flat right"
+        onClick={ () => submitSurvey(formValues) }
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   )
@@ -35,4 +43,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(SurveyFormReview)
+export default connect(mapStateToProps, actions)(SurveyFormReview)
